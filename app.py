624056fd0718d60ae9bcde6026799c02b4abb78b
Joey <48295680+Colbalt01@users.noreply.github.com>
2024-03-26 15:28:12 -0600

    Thing

--- app.py
+++ app.py
@@ -3,6 +3,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/t")
+def thing():
+    return ""
+
+song = {
+    "song_id" : "thing",
+    "name" : "The American Nightmare",
+    "":"",
+    "artist" : "Ice Nine Kills",
+    "album" : "The Silver Scream"
+}
+
+band= {
+    "band_id" : "thing",
+    "artist" : "Motionless In White"
+}
+
+album= {
+    "album_id" : "thing",
+    "artist" : "Figure",
+    "album" : "Monsters 5"
+}
